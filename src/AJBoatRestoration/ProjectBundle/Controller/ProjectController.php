daff1f7353e42d4d9bf91c65c2e33e9b592bc5b6
agrandscrutton <andygs79@googlemail.com>
2016-11-12 15:43:06 +0000

    Updating the project controller

---
+++ src/AJBoatRestoration/ProjectBundle/Controller/ProjectController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AJBoatRestoration\ProjectBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ProjectController extends Controller
+{
+    /**
+     * @Route("/project")
+     */
+    public function indexAction()
+    {
+        return $this->render('ProjectBundle:Default:index.html.twig');
+    }
+}
